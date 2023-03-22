f54660f561b349777cc46fd8840c5ac8dc996573
Toshi <toshiwu006@gmail.com>
2023-03-22 11:59:42 +0800

    feat: repack code into func gpt_QA

--- main.py
+++ main.py
@@ -40,14 +40,17 @@ def ask_gpt(message, model="gpt-3.5-turbo"):
 	return completion['choices'][0]['message']['content']
 
 def question_pos_parser(question):
-	stopSwitch, retry = False, 3
+	stopSwitch, retry = False, 1
 	mappingDict = {'noun': '名詞', 'verb': '動詞'}
+	forbidden_words = {'client_msg_id'}
 	while not stopSwitch and retry:
+		print('ask')
 		question_keywords = ask_gpt(f'Choose {min(len(question) + 2 // 3, 3)} important words from "{question}" and give me what part of speech it is. Using [word/pos] with sep by ", "').replace('\n','').replace('"','').replace("。",'')
+		print('ask_finished')
 		keyword_pos = {}
 		for k in question_keywords.split(','):
 			k = k.split('/')
-			if len(k) != 2:
+			if len(k) != 2 or k[0] in forbidden_words:
 				continue
 			keyword_pos[k[0]] = mappingDict.get(k[1], k[1])
 		stopSwitch = len(keyword_pos) > 0
@@ -80,12 +83,12 @@ def get_gpt_query(keyword, query, web_id='nineyi000360'):
 	html = f'https://www.googleapis.com/customsearch/v1/siterestrict?cx={CX[web_id]}&key={google_serch_key}&q={keyword}'
 	stopSwitch, count, result = False, 10, None
 	while not stopSwitch and count:
-		print(count)
+		print(f'第{11-count}次搜尋')
 		response = requests.get(html)
 		if response:
 			stopSwitch = response.status_code == 200
 			result = response.json().get('items')
-		count -=1
+		count -= 1
 	if not count: return '網頁錯誤'
 	if not result: return '無搜尋結果'
 	linkSet = set()
@@ -95,7 +98,7 @@ def get_gpt_query(keyword, query, web_id='nineyi000360'):
 			continue
 		url = v.get('link')
 		url = re.search(r'.+detail/[\w]+/', url).group(0) if re.search(r'.+detail/[\w]+/', url) else url
-		print(url)
+		print('搜尋結果:\t', url)
 		if url in linkSet:
 			continue
 		linkSet.add(url)
@@ -110,25 +113,70 @@ def get_gpt_query(keyword, query, web_id='nineyi000360'):
 	chatgpt_query += f"""\n\n\nCurrent date: {date}\n\nInstructions: Using the provided web search results, write a comprehensive reply to the given query. Make sure to cite results using [[number](URL)] notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.\nQuery: {query}\nReply in 繁體中文\n"""
 	return chatgpt_query
 
-def filter_ans(gpt3_ans):
+def replace_answer(gpt3_ans):
 	for url_wrong_fmt, url in re.findall(r'(<(https?:\/\/[\da-z\.-\/]+)\|.*>)', gpt3_ans):
 		gpt3_ans = gpt3_ans.replace(url_wrong_fmt, url)
 	gpt3_ans = cc.convert(gpt3_ans)
 	for url in set(re.findall(r'https?:\/\/[\w\.-\/]+', gpt3_ans)):
-		print(url)
 		gpt3_ans = gpt3_ans.replace(url, '<' + url + '|查看更多>')
-	ban_word = ['抱歉', '錯誤', '對不起']
-	for bw in ban_word:
-		if bw in gpt3_ans:
+	forbidden_words = ['抱歉', '錯誤', '對不起']
+	for w in forbidden_words:
+		if w in gpt3_ans:
 			gpt3_ans = gpt3_ans.split('，')
 			gpt3_ans = ('，').join(i for i in gpt3_ans if bw not in i)
 	gpt3_ans = '親愛的顧客您好，'+'，'.join(gpt3_ans.split('，')[1:])
 	return gpt3_ans
 
-def is_goods(keyword):
-	message = [{'role': 'user', 'content': f'幫我判斷"{keyword}"是否是一個商品,只要回答"True"或"False"'}]
-	completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=message)
-	return completion['choices'][0]['message']['content']
+def gpt_QA(message, dm_channel, user_id, ts, thread_ts, say):
+	say(text=f"請稍等為您提供回覆...", channel=dm_channel, thread_ts=ts)
+	query = f"""SELECT id, counts, question, answer, q_a_history FROM web_push.slack_chatgpt WHERE ts='{thread_ts}';"""
+	data = DBhelper('jupiter_new').ExecuteSelect(query)
+	QA_report_df = pd.DataFrame(data, columns=['id', 'counts', 'question', 'answer', 'q_a_history'])
+
+	# Step 1: get keyword from chatGPT
+	keyword_pos = question_pos_parser(message)
+	keyword = '+'.join(k.strip() for k, v in keyword_pos.items() if v == '名詞')
+	if not keyword:
+		keyword = ask_gpt(f'幫我從"{message}"選出一個重要詞彙,只要回答詞彙就好').replace('\n', '').replace('"', '').replace("。", '')
+	print('關鍵字:\t', keyword)
+
+	# Step 2: get gpt_query with search results from google search engine
+	gpt_query = get_gpt_query(keyword, message)
+	print('chatGPT輸入:\t', gpt_query)
+	if gpt_query == '網頁錯誤':
+		say(text=f"發生錯誤，請再詢問一次！", channel=dm_channel, thread_ts=ts)
+		ts_set.add(ts)
+		return
+	elif gpt_query == '無搜尋結果':
+		gpt3_answer_slack =f"親愛的顧客您好，目前無法回覆此問題，稍後將由專人為您服務。"
+		say(text=gpt3_answer_slack, channel=dm_channel, thread_ts=ts)
+	else:
+		# Step 3: response from chatGPT
+		if len(QA_report_df) > 0:
+			history = json.loads(QA_report_df['q_a_history'].iloc[0])
+			history.append({"role": "user", "content": f"{gpt_query}"})
+			while len(str(history)) > 3000 and len(history) > 3:
+				history = history[2:]
+			print('歷史紀錄:\t', history)
+			gpt3_answer = ask_gpt(history)
+		else:
+			gpt3_answer = ask_gpt(gpt_query)
+		gpt3_answer_slack = replace_answer(gpt3_answer)
+		print('cahtGPT輸出:\t', gpt3_answer_slack)
+
+	if len(QA_report_df) > 0:
+		history.append({"role": "assistant", "content": f"{gpt3_answer}"})
+		QA_report_df['counts'] += 1
+		QA_report_df[['question', 'answer', 'q_a_history']] = [gpt_query, gpt3_answer_slack, json.dumps(history)]
+	else:
+		gpt3_history = json.dumps([{"role": "user", "content": f"{gpt_query}"}, {"role": "assistant", "content": f"{gpt3_answer}"}])
+		QA_report_df = pd.DataFrame([[user_id, ts, 1, gpt_query, gpt3_answer_slack, gpt3_history, datetime.now()]],
+								  columns=['user_id', 'ts', 'counts', 'question', 'answer', 'q_a_history', 'add_time'])
+	DBhelper.ExecuteUpdatebyChunk(QA_report_df, db='jupiter_new', table='slack_chatgpt', chunk_size=100000, is_ssh=False)
+	QA_report_df = QA_report_df.drop(['q_a_history'], axis=1)
+	QA_report_df['keyword'] = keyword
+	DBhelper.ExecuteUpdatebyChunk(QA_report_df, db='jupiter_new', table='slack_chatgpt_cache', chunk_size=100000, is_ssh=False)
+	say(text=f"{gpt3_answer_slack}", channel=dm_channel, thread_ts=ts)
 
 def update_history(user_id, ts, counts, question, keyword, answer):
 	QA_report_df = pd.DataFrame([[user_id, ts, counts, question, keyword, answer, datetime.now()]],columns=['user_id', 'ts', 'counts', 'question', 'keyword', 'answer','add_time'])
@@ -195,65 +243,7 @@ def show_bert_qa(message, body, say):
 					]
 				}
 			],channel=dm_channel,thread_ts=ts)
-		else:
-			say(text=f"請稍等為您提供回覆...", channel=dm_channel,thread_ts=ts)
-			keyword_pos = question_pos_parser(text)
-			keyword = '+'.join(k.strip() for k, v in keyword_pos.items() if v == '名詞')
-			if not keyword:
-				keyword = ask_gpt(f'幫我從"{text}"選出一個重要詞彙,只要回答詞彙就好').replace('\n', '').replace('"','').replace("。", '')
-			print(keyword)
-			gpt_query = get_gpt_query(keyword, text)
-			if gpt_query == '網頁錯誤':
-				say(text=f"發生錯誤，請再詢問一次！", channel=dm_channel, thread_ts=ts)
-				ts_set.add(ts)
-				return
-			elif gpt_query == '無搜尋結果':
-				say(text=f"親愛的顧客您好，目前無法回覆此問題，稍後將由專人為您服務。", channel=dm_channel, thread_ts=ts)
-				return
-			print(gpt_query)
-			gpt3_answer	= ask_gpt(gpt_query)
-			print(gpt3_answer)
-			gpt3_answer_f = filter_ans(gpt3_answer)
-			gpt3_history = json.dumps([{"role": "user", "content": f"{gpt_query}"}, {"role": "assistant", "content":f"{gpt3_answer}"}])
-			print(gpt3_history)
-			QA_report_df = pd.DataFrame([[user_id, ts, 1, gpt_query, gpt3_answer_f, gpt3_history, datetime.now()]],columns=['user_id', 'ts', 'counts', 'last_question', 'last_answer', 'q_a_history', 'add_time'])
-			DBhelper.ExecuteUpdatebyChunk(QA_report_df, db='jupiter_new', table='slack_chatgpt', chunk_size=100000,is_ssh=False)
-			update_history(user_id, ts, 1, text, keyword, gpt3_answer_f)
-			say(text=f"{gpt3_answer_f}", channel=dm_channel,thread_ts=ts)
-
-	else:
-		say(text=f"請稍等為您提供回覆...", channel=dm_channel, thread_ts=ts)
-		query = f"""SELECT id, counts, last_question, last_answer, q_a_history FROM web_push.slack_chatgpt WHERE ts='{thread_ts}';"""
-		data = DBhelper('jupiter_new').ExecuteSelect(query)
-		df_history = pd.DataFrame(data, columns=['id', 'counts', 'last_question', 'last_answer', 'q_a_history'])
-		print(json.loads(df_history['q_a_history'].iloc[0]))
-		keyword_pos = question_pos_parser(text)
-		keyword = '+'.join(k.strip() for k, v in keyword_pos.items() if v == '名詞')
-		if not keyword:
-			keyword = ask_gpt(f'幫我從"{text}"選出一個重要詞彙,只要回答詞彙就好').replace('\n', '').replace('"', '').replace("。", '')
-		print(keyword)
-		gpt_query = get_gpt_query(keyword, text)
-		print(gpt_query)
-		if gpt_query == '網頁錯誤':
-			say(text=f"發生錯誤，請再詢問一次！", channel=dm_channel, thread_ts=ts)
-			ts_set.add(ts)
-			return
-		elif gpt_query == '無搜尋結果':
-			say(text=f"親愛的顧客您好，目前無法回覆此問題，稍後將由專人為您服務。", channel=dm_channel, thread_ts=ts)
-			return
-		history = json.loads(df_history['q_a_history'].iloc[0])
-		history.append({"role": "user", "content": f"{gpt_query}"})
-		print(history)
-		while len(str(history)) > 3000 and len(history) > 3:
-			history = history[2:]
-		gpt3_answer	= ask_gpt(history)
-		gpt3_answer_f = filter_ans(gpt3_answer)
-		history.append({"role": "assistant", "content": f"{gpt3_answer}"})
-		df_history['counts'] += 1
-		df_history[['last_question', 'last_answer', 'q_a_history']] = [gpt_query, gpt3_answer_f ,json.dumps(history)]
-		DBhelper.ExecuteUpdatebyChunk(df_history, db='jupiter_new', table='slack_chatgpt', chunk_size=100000, is_ssh=False)
-		update_history(user_id, ts, 1, text, keyword, gpt3_answer_f)
-		say(text=f"{gpt3_answer_f}", channel=dm_channel, thread_ts=ts)
+	gpt_QA(text, dm_channel, user_id, ts, thread_ts, say)
 	return
 
 @app.action("bo1")
